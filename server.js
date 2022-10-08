5328b6d44362d8bfa5c883ff1c717a53a158ec5a
MVsingularity <60405343+MVsingularity@users.noreply.github.com>
2022-10-08 14:48:55 -0700

    figuring out ports and migrations

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const { engine } = require('express-handlebars');
 
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true}));
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
@@ -13,7 +15,7 @@ app.get('/', (req, res) => {
 });
 app.use("/images", express.static(path.join(__dirname, '/images')));
 
-//donor page
+//charity page
 
 //new
 app.get('/charities/new', (req, res) => {
@@ -25,15 +27,15 @@ app.get('/charities/new', (req, res) => {
 app.post('/charities', (req, res) => {
   console.log("success")
 
-  const charitiesId = "2"
-  res.redirect('/charities/${charitiesId}')
+  const charityId = "2";
+  res.redirect('/charities/${charityId}')
 
 });
-
+// show
 app.get('/charities/:id', (req, res) => {
   console.log(req.params.id);
 
-  res.render('charities-show', { charities } )
+  res.render('charities-show', { charity })
 });
 //edit
 app.get('/charities/:id/edit', (req, res) => {
@@ -45,6 +47,38 @@ app.put('/charities/:id', (req, res) => {
 })
 //destroy
 app.delete('/charities/:id', (req, res) => {
+
+})
+
+//donor page
+app.get('/donors/new', (req, res) => {
+  res.render('donors-new', {});
+})
+
+//create
+app.post('/donors', (req, res) => {
+  console.log("success")
+
+  const charityId = "2";
+  res.redirect('/donors/${donorId}')
+
+});
+// show
+//app.get('/donors/:id', (req, res) => {
+//  console.log(req.params.id);
+
+  //res.render('donors-show', { charity })
+//});
+//edit
+//app.get('/donors/:id/edit', (req, res) => {
+
+//})
+//update
+//app.put('/donors/:id', (req, res) => {
+
+//})
+//destroy
+app.delete('/donors/:id', (req, res) => {
 
 })
 app.listen(3000);
